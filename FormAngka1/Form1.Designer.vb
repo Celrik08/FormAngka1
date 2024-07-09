e78582653f40deb17000768560cb124fa654026b
Celrik08 <m.fajar080907@gmail.com>
2024-07-09 12:03:14 +0800

    Commit1

--- FormAngka1/Form1.Designer.vb
+++ FormAngka1/Form1.Designer.vb
@@ -22,10 +22,285 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        TextNilai1 = New TextBox()
+        TextNilai2 = New TextBox()
+        TextNilai3 = New TextBox()
+        TextNilai4 = New TextBox()
+        TextNilai5 = New TextBox()
+        TextNilai6 = New TextBox()
+        TextNilai7 = New TextBox()
+        TextNilai14 = New TextBox()
+        TextNilai13 = New TextBox()
+        TextNilai12 = New TextBox()
+        TextNilai11 = New TextBox()
+        TextNilai10 = New TextBox()
+        TextNilai9 = New TextBox()
+        TextNilai8 = New TextBox()
+        ComboPilihan = New ComboBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(13, 15)
+        Label1.Margin = New Padding(4, 0, 4, 0)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(56, 20)
+        Label1.TabIndex = 0
+        Label1.Text = "Nilai 1"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(13, 49)
+        Label2.Margin = New Padding(4, 0, 4, 0)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(56, 20)
+        Label2.TabIndex = 1
+        Label2.Text = "Nilai 2"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(13, 83)
+        Label3.Margin = New Padding(4, 0, 4, 0)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(56, 20)
+        Label3.TabIndex = 2
+        Label3.Text = "Nilai 3"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(13, 117)
+        Label4.Margin = New Padding(4, 0, 4, 0)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(56, 20)
+        Label4.TabIndex = 3
+        Label4.Text = "Nilai 4"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(13, 151)
+        Label5.Margin = New Padding(4, 0, 4, 0)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(56, 20)
+        Label5.TabIndex = 4
+        Label5.Text = "Nilai 5"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(13, 185)
+        Label6.Margin = New Padding(4, 0, 4, 0)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(56, 20)
+        Label6.TabIndex = 5
+        Label6.Text = "Nilai 6"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(13, 219)
+        Label7.Margin = New Padding(4, 0, 4, 0)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(56, 20)
+        Label7.TabIndex = 6
+        Label7.Text = "Nilai 7"
+        ' 
+        ' TextNilai1
+        ' 
+        TextNilai1.Location = New Point(76, 12)
+        TextNilai1.Name = "TextNilai1"
+        TextNilai1.Size = New Size(125, 28)
+        TextNilai1.TabIndex = 7
+        ' 
+        ' TextNilai2
+        ' 
+        TextNilai2.Location = New Point(76, 46)
+        TextNilai2.Name = "TextNilai2"
+        TextNilai2.Size = New Size(125, 28)
+        TextNilai2.TabIndex = 8
+        ' 
+        ' TextNilai3
+        ' 
+        TextNilai3.Location = New Point(76, 80)
+        TextNilai3.Name = "TextNilai3"
+        TextNilai3.Size = New Size(125, 28)
+        TextNilai3.TabIndex = 9
+        ' 
+        ' TextNilai4
+        ' 
+        TextNilai4.Location = New Point(76, 114)
+        TextNilai4.Name = "TextNilai4"
+        TextNilai4.Size = New Size(125, 28)
+        TextNilai4.TabIndex = 10
+        ' 
+        ' TextNilai5
+        ' 
+        TextNilai5.Location = New Point(76, 148)
+        TextNilai5.Name = "TextNilai5"
+        TextNilai5.Size = New Size(125, 28)
+        TextNilai5.TabIndex = 11
+        ' 
+        ' TextNilai6
+        ' 
+        TextNilai6.Location = New Point(76, 182)
+        TextNilai6.Name = "TextNilai6"
+        TextNilai6.Size = New Size(125, 28)
+        TextNilai6.TabIndex = 12
+        ' 
+        ' TextNilai7
+        ' 
+        TextNilai7.Location = New Point(76, 216)
+        TextNilai7.Name = "TextNilai7"
+        TextNilai7.Size = New Size(125, 28)
+        TextNilai7.TabIndex = 13
+        ' 
+        ' TextNilai14
+        ' 
+        TextNilai14.Location = New Point(272, 216)
+        TextNilai14.Name = "TextNilai14"
+        TextNilai14.Size = New Size(125, 28)
+        TextNilai14.TabIndex = 20
+        ' 
+        ' TextNilai13
+        ' 
+        TextNilai13.Location = New Point(272, 182)
+        TextNilai13.Name = "TextNilai13"
+        TextNilai13.Size = New Size(125, 28)
+        TextNilai13.TabIndex = 19
+        ' 
+        ' TextNilai12
+        ' 
+        TextNilai12.Location = New Point(272, 148)
+        TextNilai12.Name = "TextNilai12"
+        TextNilai12.Size = New Size(125, 28)
+        TextNilai12.TabIndex = 18
+        ' 
+        ' TextNilai11
+        ' 
+        TextNilai11.Location = New Point(272, 114)
+        TextNilai11.Name = "TextNilai11"
+        TextNilai11.Size = New Size(125, 28)
+        TextNilai11.TabIndex = 17
+        ' 
+        ' TextNilai10
+        ' 
+        TextNilai10.Location = New Point(272, 80)
+        TextNilai10.Name = "TextNilai10"
+        TextNilai10.Size = New Size(125, 28)
+        TextNilai10.TabIndex = 16
+        ' 
+        ' TextNilai9
+        ' 
+        TextNilai9.Location = New Point(272, 46)
+        TextNilai9.Name = "TextNilai9"
+        TextNilai9.Size = New Size(125, 28)
+        TextNilai9.TabIndex = 15
+        ' 
+        ' TextNilai8
+        ' 
+        TextNilai8.Location = New Point(272, 12)
+        TextNilai8.Name = "TextNilai8"
+        TextNilai8.Size = New Size(125, 28)
+        TextNilai8.TabIndex = 14
+        ' 
+        ' ComboPilihan
+        ' 
+        ComboPilihan.FormattingEnabled = True
+        ComboPilihan.Location = New Point(165, 275)
+        ComboPilihan.Name = "ComboPilihan"
+        ComboPilihan.Size = New Size(151, 28)
+        ComboPilihan.TabIndex = 21
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(22, 334)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 29)
+        Button1.TabIndex = 22
+        Button1.Text = "Klik"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(370, 334)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(94, 29)
+        Button2.TabIndex = 23
+        Button2.Text = "Reset"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(635, 404)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(ComboPilihan)
+        Controls.Add(TextNilai14)
+        Controls.Add(TextNilai13)
+        Controls.Add(TextNilai12)
+        Controls.Add(TextNilai11)
+        Controls.Add(TextNilai10)
+        Controls.Add(TextNilai9)
+        Controls.Add(TextNilai8)
+        Controls.Add(TextNilai7)
+        Controls.Add(TextNilai6)
+        Controls.Add(TextNilai5)
+        Controls.Add(TextNilai4)
+        Controls.Add(TextNilai3)
+        Controls.Add(TextNilai2)
+        Controls.Add(TextNilai1)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Font = New Font("Times New Roman", 10.8F, FontStyle.Regular, GraphicsUnit.Point)
+        Margin = New Padding(4, 3, 4, 3)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
 
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TextNilai1 As TextBox
+    Friend WithEvents TextNilai2 As TextBox
+    Friend WithEvents TextNilai3 As TextBox
+    Friend WithEvents TextNilai4 As TextBox
+    Friend WithEvents TextNilai5 As TextBox
+    Friend WithEvents TextNilai6 As TextBox
+    Friend WithEvents TextNilai7 As TextBox
+    Friend WithEvents TextNilai14 As TextBox
+    Friend WithEvents TextNilai13 As TextBox
+    Friend WithEvents TextNilai12 As TextBox
+    Friend WithEvents TextNilai11 As TextBox
+    Friend WithEvents TextNilai10 As TextBox
+    Friend WithEvents TextNilai9 As TextBox
+    Friend WithEvents TextNilai8 As TextBox
+    Friend WithEvents ComboPilihan As ComboBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
 End Class
--- FormAngka1/Form1.vb
+++ FormAngka1/Form1.vb
@@ -1,3 +1,148 @@
 ﻿Public Class Form1
 
+    Private Sub TextNilai1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextNilai1.KeyPress
+        ' Cek apakah karakter yang dimasukkan adalah angka atau kontrol seperti Backspace atau Delete
+        If Not Char.IsDigit(e.KeyChar) AndAlso Not Char.IsControl(e.KeyChar) Then
+            ' Jika bukan angka atau kontrol, batalkan input dengan menandai event sebagai sudah di-handle
+            e.Handled = True
+        End If
+
+        If e.KeyChar = ChrW(Keys.Enter) Then
+            TextNilai2.Focus()
+        End If
+    End Sub
+
+    Private Sub TextNilai2_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextNilai2.KeyPress
+        ' Cek apakah karakter yang dimasukkan adalah angka atau kontrol seperti Backspace atau Delete
+        If Not Char.IsDigit(e.KeyChar) AndAlso Not Char.IsControl(e.KeyChar) Then
+            ' Jika bukan angka atau kontrol, batalkan input dengan menandai event sebagai sudah di-handle
+            e.Handled = True
+        End If
+
+        If e.KeyChar = ChrW(Keys.Enter) Then
+            TextNilai3.Focus()
+        End If
+    End Sub
+
+    Private Sub TextNilai3_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextNilai3.KeyPress
+        ' Cek apakah karakter yang dimasukkan adalah angka atau kontrol seperti Backspace atau Delete
+        If Not Char.IsDigit(e.KeyChar) AndAlso Not Char.IsControl(e.KeyChar) Then
+            ' Jika bukan angka atau kontrol, batalkan input dengan menandai event sebagai sudah di-handle
+            e.Handled = True
+        End If
+
+        If e.KeyChar = ChrW(Keys.Enter) Then
+            TextNilai4.Focus()
+        End If
+    End Sub
+
+    Private Sub TextNilai4_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextNilai4.KeyPress
+        ' Cek apakah karakter yang dimasukkan adalah angka atau kontrol seperti Backspace atau Delete
+        If Not Char.IsDigit(e.KeyChar) AndAlso Not Char.IsControl(e.KeyChar) Then
+            ' Jika bukan angka atau kontrol, batalkan input dengan menandai event sebagai sudah di-handle
+            e.Handled = True
+        End If
+
+        If e.KeyChar = ChrW(Keys.Enter) Then
+            TextNilai5.Focus()
+        End If
+    End Sub
+
+    Private Sub TextBox5_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextNilai5.KeyPress
+        ' Cek apakah karakter yang dimasukkan adalah angka atau kontrol seperti Backspace atau Delete
+        If Not Char.IsDigit(e.KeyChar) AndAlso Not Char.IsControl(e.KeyChar) Then
+            ' Jika bukan angka atau kontrol, batalkan input dengan menandai event sebagai sudah di-handle
+            e.Handled = True
+        End If
+
+        If e.KeyChar = ChrW(Keys.Enter) Then
+            TextNilai6.Focus()
+        End If
+    End Sub
+
+    Private Sub TextNilai6_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextNilai6.KeyPress
+        ' Cek apakah karakter yang dimasukkan adalah angka atau kontrol seperti Backspace atau Delete
+        If Not Char.IsDigit(e.KeyChar) AndAlso Not Char.IsControl(e.KeyChar) Then
+            ' Jika bukan angka atau kontrol, batalkan input dengan menandai event sebagai sudah di-handle
+            e.Handled = True
+        End If
+
+        If e.KeyChar = ChrW(Keys.Enter) Then
+            TextNilai7.Focus()
+        End If
+    End Sub
+
+    Private Sub TextBox7_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextNilai7.KeyPress
+        ' Cek apakah karakter yang dimasukkan adalah angka atau kontrol seperti Backspace atau Delete
+        If Not Char.IsDigit(e.KeyChar) AndAlso Not Char.IsControl(e.KeyChar) Then
+            ' Jika bukan angka atau kontrol, batalkan input dengan menandai event sebagai sudah di-handle
+            e.Handled = True
+        End If
+
+        If e.KeyChar = ChrW(Keys.Enter) Then
+            TextNilai7.Focus()
+            e.Handled = True
+            AngkaRandom()
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        AngkaRandom()
+    End Sub
+
+    Private Sub AngkaRandom()
+        ' Mengumpulkan nilai-nilai dari TextBox ke dalam array
+        Dim values(6) As Integer ' Array dengan 7 elemen (indeks 0-6)
+
+        If Integer.TryParse(TextNilai1.Text, values(0)) AndAlso
+           Integer.TryParse(TextNilai2.Text, values(1)) AndAlso
+           Integer.TryParse(TextNilai4.Text, values(2)) AndAlso
+           Integer.TryParse(TextNilai3.Text, values(3)) AndAlso
+           Integer.TryParse(TextNilai5.Text, values(4)) AndAlso
+           Integer.TryParse(TextNilai6.Text, values(5)) AndAlso
+           Integer.TryParse(TextNilai7.Text, values(6)) Then
+
+            ' Mengurutkan array sesuai dengan pilihan ComboBox
+            If ComboPilihan.SelectedIndex = 0 Then
+                Array.Sort(values)
+            ElseIf ComboPilihan.SelectedIndex = 1 Then
+                Array.Sort(values, New Comparison(Of Integer)(Function(x, y) y.CompareTo(x)))
+            End If
+
+            ' Menetapkan nilai-nilai yang diurutkan kembali ke TextBox
+            TextNilai8.Text = values(0).ToString()
+            TextNilai9.Text = values(1).ToString()
+            TextNilai10.Text = values(2).ToString()
+            TextNilai11.Text = values(3).ToString()
+            TextNilai12.Text = values(4).ToString()
+            TextNilai13.Text = values(5).ToString()
+            TextNilai14.Text = values(6).ToString()
+        Else
+            MessageBox.Show("Masukkan angka yang valid di semua TextBox.", "Kesalahan", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Inisialisasi ComboBox
+        ComboPilihan.Items.Add("Terkecil ke Terbesar")
+        ComboPilihan.Items.Add("Terbesar ke Terkecil")
+        ComboPilihan.SelectedIndex = 0 ' Pilihan default
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextNilai1.Text = ""
+        TextNilai2.Text = ""
+        TextNilai3.Text = ""
+        TextNilai4.Text = ""
+        TextNilai5.Text = ""
+        TextNilai6.Text = ""
+        TextNilai7.Text = ""
+        TextNilai8.Text = ""
+        TextNilai9.Text = ""
+        TextNilai10.Text = ""
+        TextNilai11.Text = ""
+        TextNilai12.Text = ""
+        TextNilai13.Text = ""
+        TextNilai14.Text = ""
+    End Sub
 End Class
